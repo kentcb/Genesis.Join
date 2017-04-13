d37069a99a5cafd8396f703d588d5f37d483bf67
Kent Boogaart <kent.boogaart@gmail.com>
2017-04-13 09:49:54 +0930

    Migrate to netstandard

--- Src/Genesis.Join/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Genesis.Join")]
-[assembly: AssemblyDescription("Contains the implementation of Genesis.Join.")]
-[assembly: CLSCompliantAttribute(true)]
